774d2f6abe8574b754aa873875adb92d148ea10f
mash <brian.macharia731@gmail.com>
2024-04-22 00:45:27 +0300

    impoved version with ui

--- fertilizer-tracker/src/App.js
+++ fertilizer-tracker/src/App.js
@@ -1,27 +1,29 @@
-import "./input.css"
-import React, { useState } from 'react';
+import "./input.css";
+import React, { useState } from "react";
 
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
-import DistributorDash from './components/Distributor';
-import Home from './components/Home';
-import ParentComponent from "./components/ParentComponent";
-import FertilizerDataForm from "./components/FertilizerDataForm";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import DistributorDash from "./components/Distributor";
+import Home from "./components/Home";
+
+import FertilizerTransfer from "./components/FertilizerTransfer";
+import PendingOrders from "./components/PendingOrders";
+import AllFertilizers from "./components/AllFertilizers";
 
 function App() {
   const [showScanner, setShowScanner] = useState(false);
 
   return (
     <div className="App">
-     
       <Router>
         <Switch>
-        <Route exact path='/' component={Home}/>
-          <Route path='/dashboard' component={DistributorDash}/>
-          <Route path='/form' component={ParentComponent}/>
-          <Route path='/fertilizerdata' component={FertilizerDataForm}/>
+          <Route exact path="/" component={Home} />
+          <Route path="/dashboard" component={DistributorDash} />
+
+          <Route path="/fertilizerdata" component={FertilizerTransfer} />
+          <Route path="/pending" component={PendingOrders} />
+          <Route path="/allfertilizers" component={AllFertilizers} />
         </Switch>
       </Router>
-      
     </div>
   );
 }
--- fertilizer-tracker/src/components/QrReader.js
+++ fertilizer-tracker/src/components/QrReader.js
@@ -1,35 +1,83 @@
 import React, { useEffect, useRef, useState } from "react";
 import QrScanner from "qr-scanner";
 import QrFrame from "../assets/qr-frame.svg";
-import "./QrStyles.css";
-import Modal from 'react-modal';
+import axios from "axios";
 
-Modal.setAppElement('#root') // replace '#root' with the id of the root element of your app
-
-const QrReader = ({ isOpen, onRequestClose }) => {
+const QrReader = ({ stopScanning, onScanSuccess }) => {
   const scanner = useRef(null);
   const videoEl = useRef(null);
   const qrBoxEl = useRef(null);
   const [qrOn, setQrOn] = useState(false);
-  const [scannedResult, setScannedResult] = useState("");
+  const [formData, setFormData] = useState({
+    fertilizerName: "",
+    fertilizerId: "",
+    fertilizerManufacturer: "",
+  });
+  const [showScanner, setShowScanner] = useState(true);
 
   const handleScanSuccess = (result) => {
-    // If the result is already an object, you can directly access its properties
-    setScannedResult(result);
-    scanner.current.stop();
-    if (videoEl.current && videoEl.current.srcObject) {
-      videoEl.current.srcObject.getTracks().forEach(track => track.stop());
-    }
-    setQrOn(false);
-    onRequestClose();
+    onScanSuccess(result);
+    let parsedResult = JSON.parse(result.data);
+    setFormData({
+      fertilizerName: parsedResult.fertilizer_type,
+      fertilizerId: parsedResult.fertilizer_id,
+      fertilizerManufacturer: parsedResult.fertilizer_manufacturer,
+    });
+    setShowScanner(false);
   };
 
   const onScanFail = (err) => {
     console.log(err);
   };
 
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setFormData({
+      ...formData,
+      [id]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("Form submitted with data:", formData);
+
+    try {
+      // Make API call to create fertilizer
+      const response = await axios.post(
+        "http://localhost:8080/api/addfertilizer",
+        {
+          fertilizer_type: formData.fertilizerName,
+          fertilizer_id: formData.fertilizerId,
+          fertilizer_manufacturer: formData.fertilizerManufacturer,
+        }
+      );
+
+      if (response.status === 200) {
+        console.log("Fertilizer created successfully:", response.data);
+
+        // Hide the scanner and reset form data
+        setShowScanner(false);
+        setFormData({
+          fertilizer_name: "",
+          fertilizer_id: "",
+          fertilizer_manufacturer: "",
+        });
+
+        // Refresh the page
+        window.location.reload();
+      } else {
+        console.error("Failed to create fertilizer:", response.data);
+        alert("Failed to create fertilizer. Please try again.");
+      }
+    } catch (error) {
+      console.error("Error creating fertilizer:", error);
+      alert("An error occurred. Please try again.");
+    }
+  };
+
   useEffect(() => {
-    if (videoEl?.current && !scanner.current) {
+    if (videoEl?.current && !scanner.current && showScanner) {
       scanner.current = new QrScanner(videoEl?.current, handleScanSuccess, {
         onDecodeError: onScanFail,
         preferredCamera: "environment",
@@ -51,7 +99,7 @@ const QrReader = ({ isOpen, onRequestClose }) => {
         scanner?.current?.stop();
       }
     };
-  }, []);
+  }, [showScanner]);
 
   useEffect(() => {
     if (!qrOn)
@@ -61,20 +109,83 @@ const QrReader = ({ isOpen, onRequestClose }) => {
   }, [qrOn]);
 
   return (
-    <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
-      <div className="qr-reader">
-        <video ref={videoEl}></video>
-        <div ref={qrBoxEl} className="qr-box">
-          <img
-            src={QrFrame}
-            alt="Qr Frame"
-            width={256}
-            height={256}
-            className="qr-frame"
-          />
-        </div>
-      </div>
-    </Modal>
+    <div className="qr-reader">
+      {showScanner && (
+        <>
+          <video ref={videoEl}></video>
+          <div ref={qrBoxEl} className="qr-box">
+            <img
+              src={QrFrame}
+              alt="Qr Frame"
+              width={256}
+              height={256}
+              className="qr-frame"
+            />
+          </div>
+        </>
+      )}
+      {!showScanner && (
+        <form
+          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+          onSubmit={handleSubmit}
+        >
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="fertilizerName"
+            >
+              Fertilizer Name
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="fertilizerName"
+              type="text"
+              value={formData.fertilizerName}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="fertilizerId"
+            >
+              Fertilizer ID
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="fertilizerId"
+              type="text"
+              value={formData.fertilizerId}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="fertilizerManufacturer"
+            >
+              Fertilizer Manufacturer
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="fertilizerManufacturer"
+              type="text"
+              value={formData.fertilizerManufacturer}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="mb-4">
+            <button
+              type="submit"
+              className="mt-3 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      )}
+    </div>
   );
 };
 
